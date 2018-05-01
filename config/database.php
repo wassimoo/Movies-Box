6b28e3a215f492bb941ac657bc5ffd16e53bc0ab
wassimoo <wassimjbaali5121997@gmail.com>
2018-05-01 16:21:31 +0100

    database configuration.

---
+++ config/database.php
@@ -0,0 +1,31 @@
+<?php 
+
+/**
+ * Database Hostname
+ */
+define("HOST", "localhost/XE");
+
+/**
+ * Database connection port
+ */
+define("PORT", "1521");
+
+/**
+ * Data charset - default utf-8
+ */
+define("CHARSET", "AL32UTF8");
+
+/**
+ * Connection schema, Empty means db users default
+ */
+define("SCHEMA", "");
+
+/**
+ * Database user identifier
+ */
+define("USERNAME", "moviesbox");
+
+/**
+ * Database user password
+ */
+define("PASSWORD", "waelo");
