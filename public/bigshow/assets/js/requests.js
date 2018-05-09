790999abda77c778f249b1e1fad960bbec1ecc30
wassimoo <wassimjbaali5121997@gmail.com>
2018-05-09 15:22:25 +0100

    signup info retrival by id.

--- public/bigshow/assets/js/requests.js
+++ public/bigshow/assets/js/requests.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
     $("#login").click(function (e) {
         e.preventDefault();
-        
+
         var username = $("#username").val();
         var password = $("#password").val();
         var stayLoggedIn = $("#stayIn").prop('checked');
 
         var msg = $(".login-msg");
 
-        if(username == ""){
+        if (username == "") {
             msg.html("please specify a username");
             msg.show();
             return;
         }
-        if(password == ""){
+        if (password == "") {
             msg.html("please enter a password");
             msg.show();
             return;
@@ -22,24 +22,25 @@ $(document).ready(function () {
         $.ajax({
             url: "login/performLogin",
             type: "POST",
-            data: { username: username,
-                    password: password ,
-                    stayLoggedIn: stayLoggedIn
-                },
+            data: {
+                username: username,
+                password: password,
+                stayLoggedIn: stayLoggedIn
+            },
             dataType: "html",
-            beforeSend: function(){
+            beforeSend: function () {
                 msg.html("Logging you in ...");
                 msg.show();
             },
             success: function (html) {
                 if (html == "true")
                     window.location.replace(" ");
-                else{
-                    if(html == "can't establish connection to server"){
+                else {
+                    if (html == "can't establish connection to server") {
                         msg.html(html);
                         msg.show();
                     }
-                    else{
+                    else {
                         msg.html("Invalid username/password !");
                         msg.show();
                     }
@@ -47,4 +48,55 @@ $(document).ready(function () {
             }
         })
     });
+
+    $("#register").click(function (e) {
+        e.preventDefault();
+
+        var msg = $(".login-msg");
+
+        var data = [
+            $("#username").val(),
+            $("#password").val(),
+            $("#first_name").val(),
+            $("#last_name").val(),
+            $("#email").val()
+        ]
+
+        data.forEach(info => {
+            if (info == "")
+                msg.html("please provide all required informations");
+            msg.show();
+            return;
+        });
+
+        $.ajax({
+            url: "signup/performsignup",
+            type: "POST",
+            data: {
+                username: data[0],
+                password: data[1],
+                first_name: data[2],
+                last_name: data[3],
+                email: data[4]
+            },
+            dataType: "html",
+            beforeSend: function () {
+                msg.html("Signing you up ...");
+                msg.show();
+            },
+            success: function (html) {
+                if (html == "true")
+                    window.location.replace(" ");
+                else if (html == "can't establish connection to server") {
+                    msg.html(html);
+                    msg.show();
+                }
+                else {
+                    msg.html("Inknown error occured !");
+                    msg.show();
+                }
+            }
+            })
+
+    })
 });
