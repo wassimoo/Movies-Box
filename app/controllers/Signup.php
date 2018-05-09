2e62e156cd439438ec6dac4de2c79e756f4e2931
wassimoo <wassimjbaali5121997@gmail.com>
2018-05-09 15:20:23 +0100

    model based signup.

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -1,19 +1,47 @@
 <?php
 
 use AlphaLayout\Renderer;
+use AlphaSession\Session;
+use MoviesBox\DBCException;
+use MoviesBox\User;
 
+require APP_PATH . "/models/User.php";
 
 class Signup
 {
     public static function control()
     {
+        session_start();
         self::showPage();
     }
 
     private static function showPage(){
+        if(isset($_SESSION["info"]) && $_SESSION["info"]->validate()){
+            header("Location:" . DOMAIN);
+        }
         $renderer = new Renderer(TEMPLATE_DIR);
        $page = $renderer->render("signup.html",["domain" => DOMAIN]);
        echo  $renderer->render("simple.html", ["main" => $page]);
  
+    }
+
+    public static function performSignup(){
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $first_name = $_POST["first_name"];
+        $last_name = $_POST["last_name"];
+
+        $user = new User($username, $first_name, $last_name, $email, $password);
+        try{
+            if($user->register()){
+                $_SESSION["info"] = new Session();
+                $_SESSION["info"]->data = ["user" => $user];
+                echo "true";
+            }
+        }catch(DBCException $ex){
+            echo "can't establish connection to server";
+
+        }
     }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -54,11 +54,11 @@ class User
     }
 
     /**
-	 * Perform database check for login process.
-     * @param string $username 
-	 * @param string $password
-	 * @return mixed false or User instance
-	 * @throws DBCException can't establish connection
+     * Perform database check for login process.
+     * @param string $username
+     * @param string $password
+     * @return mixed false or User instance
+     * @throws DBCException can't establish connection
      */
     public static function login(string $username, string $password)
     {
@@ -66,54 +66,92 @@ class User
         $db->connect(USERNAME, PASSWORD);
 
         $query = 'SELECT password
-                    FROM "user"
+                    FROM user_
                   WHERE username = :username';
-		$result = $db->qin($query, ["username" => $username]);
-		if(($result = self::fetch($result)) == false)
-			return false;
-		
-		if (password_verify($password, $result[0]["PASSWORD"]))
-		{
-			$query = 'SELECT NAME, LAST_NAME, EMAIL FROM "user" WHERE USERNAME = :username';
-			$result = $db->qin($query, ["username"=>$username]);
-			if(($result = self::fetch($result)) == false)
-				return false; // no sense ? 
-			$first_name = $result[0]["NAME"];
-			$last_name = $result[0]["LAST_NAME"];
-			$email = $result[0]["EMAIL"];
+        $result = $db->qin($query, ["username" => $username]);
+        if (($result = self::fetch($result)) == false) {
+            return false;
+        }
 
-			return new User($username, $first_name, $last_name, $email, "");
-		}else{
-			return false;
-		}
+        if (password_verify($password, $result[0]["PASSWORD"])) {
+            $query = 'SELECT NAME, LAST_NAME, EMAIL FROM "user" WHERE USERNAME = :username';
+            $result = $db->qin($query, ["username" => $username]);
+            if (($result = self::fetch($result)) == false) {
+                return false;
+            }
+            // no sense ?
+            $first_name = $result[0]["NAME"];
+            $last_name = $result[0]["LAST_NAME"];
+            $email = $result[0]["EMAIL"];
+
+            return new User($username, $first_name, $last_name, $email, "");
+        } else {
+            return false;
+        }
     }
 
     /**
-     *
+     * @param bool $asAdmin by default user is added as client unless $asAdmin = true
      */
-    public function register(): bool
+    public function register(bool $asAdmin = false): bool
     {
         $db = new Oracle();
-		$db->connect(USERNAME, PASSWORD);
-		$query = 'INSERT into "user" VALUES (
-			:username,
-			:password,
-			:first_name,
-			:last_name,
-			:email
-		  )';
-		$data = [
-			"username" => $this->username,
-			"password" => $this->password,
-			"first_name"=> $this->first_name,
-			"last_name" => $this->last_name,
-			"email" => $this->email
-		];
-		$result = $db->qout($query, $data);
-		if($result["success"] == false){
-			throw new DBCException($result["data"], 1);
-		}
-		return true;
+        $db->connect(USERNAME, PASSWORD);
+
+        /****************** Username existance ***********************/
+        $query = 'SELECT Count(username) EXIST
+					FROM user_
+				WHERE username = :username';
+        $result = $db->qin($query, [":username" => $this->username]);
+        if (($result = self::fetch($result)) == false) {
+            return false;
+        } else if ($result[0]["EXIST"] == 1) {
+            throw new DBCException("username already exists");
+        }
+        /************************************************************/
+
+        /***************** User register  ***************************/
+        $query = "INSERT INTO user_ VALUES( :username, :pwd, :first_name, :last_name, :email)";
+        $data = [
+            ":username" => $this->username,
+            ":pwd" => password_hash($this->password, PASSWORD_BCRYPT),
+            ":first_name" => $this->first_name,
+            ":last_name" => $this->last_name,
+            ":email" => $this->email,
+        ];
+        $result = $db->qout($query, $data);
+        if ($result["success"] == false) {
+            throw new DBCException($result["data"]);
+        }
+
+        /*************************************************************/
+
+        if ($asAdmin == false) {
+            return $this->registerByRole($db, 'client');
+        } else {
+            return $this->registerByRole($db, 'admin');
+        }
+
+    }
+
+    /**
+     * Used to register Client/Admin
+     * @param Oracle $db oracleDB instance
+     * @param string $role is actually the tableName client/admin
+     * @throws DBCException
+     */
+    private function registerByRole(Oracle $db, String $role)
+    {
+        $query = "INSERT into $role(username) VALUES(
+			:username
+		  )";
+
+        $result = $db->qout($query, ["username" => $this->username]);
+        if ($result["success"] == false) {
+            throw new DBCException($result["data"]);
+        }
+        $this->password=""; //TODO: in case of object serialisation.
+        return true;
     }
 
     /**
@@ -131,21 +169,20 @@ class User
     public function add_video(): void
     {
         // TODO: implement here
-	}
+    }
 
-	/**
-	 * 
-	 * @throws DBCException 
-	 */
-	public static function fetch(array $result){
-		if(empty($result["success"])){
-			throw new DBCException("no result found");
-		}
-		else if(empty($result["data"])){
-			return false;
-		}
-		else{
-			return $result["data"];
-		}
-	}
+    /**
+     *
+     * @throws DBCException
+     */
+    public static function fetch(array $result)
+    {
+        if (empty($result["success"])) {
+            throw new DBCException("no result found");
+        } else if (empty($result["data"])) {
+            return false;
+        } else {
+            return $result["data"];
+        }
+    }
 }
