919eb75961a9a5ee246050808e1b5ed2a6142067
wassimoo <wassimjbaali5121997@gmail.com>
2018-05-09 06:33:08 +0100

    Login based on User model.

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -1,22 +1,24 @@
 <?php
 
 use AlphaLayout\Renderer;
-use MoviesBox\DBCException;
-use MoviesBox\Oracle;
 use AlphaSession\Session;
- 
+use MoviesBox\DBCException;
+use MoviesBox\User;
+
+require APP_PATH . "/models/User.php";
+
 class Login
 {
     public static function control()
     {
         session_start();
-        //session_destroy();
+        session_destroy();
         self::showPage();
     }
 
     private static function showPage()
     {
-        if(isset($_SESSION["info"]) && $_SESSION["info"]->validate()){
+        if (isset($_SESSION["info"]) && $_SESSION["info"]->validate()) {
             header("Location:" . DOMAIN);
         }
 
@@ -30,35 +32,18 @@ class Login
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-        $db = new Oracle();
         try {
-            $db->connect(USERNAME, PASSWORD);
+            if (($user = User::login($username, $password))== false) {
+                echo "false";
+            }else{
+                $_SESSION["info"] = new Session();
+                $_SESSION["info"]->data = ["user" => $user];
+                echo "true";
+            }
         } catch (DBCException $ex) {
             echo "can't establish connection to server";
             return;
         }
 
-        $hash = password_hash($password, PASSWORD_BCRYPT);
-
-        $query = "SELECT password
-                    FROM client
-                  WHERE username = :username";
-        $result = $db->qin($query, ["username" => $username]);
-
-        if ($result["success"] == false) {
-            echo "false";
-            return;
-        } else {
-            $result = $result["data"];
-        }
-
-        if (password_verify($password, $result[0]["PASSWORD"]) == false) {
-            echo "false";
-            return;
-        }
-
-        $_SESSION["info"] = new Session();
-        $_SESSION["info"]->data = ["username" => $username];
-        echo "true";
     }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -1,89 +1,151 @@
 <?php
 
+namespace MoviesBox;
 
 /**
  *
  */
 class User
 {
-	/**
-	 *
-	 */
-	public function __construct(string $username, string $first_name, string $last_name, string $email, string $password)
-	{
-		$this->username = $username;
-		$this->first_name = $first_name;
-		$this->last_name = $last_name;
-		$this->email = $email;
-		$this->password = $password;
+
+    /**
+     * @var string
+     */
+    public $username;
+
+    /**
+     * @var string
+     */
+    private $first_name;
+
+    /**
+     * @var string
+     */
+    private $last_name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     *
+     */
+    public function __construct(string $username, string $first_name, string $last_name, string $email, string $password)
+    {
+        $this->username = $username;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    /**
+     *
+     */
+    public function search(): void
+    {
+        // TODO: implement here
+    }
+
+    /**
+	 * Perform database check for login process.
+     * @param string $username 
+	 * @param string $password
+	 * @return mixed false or User instance
+	 * @throws DBCException can't establish connection
+     */
+    public static function login(string $username, string $password)
+    {
+        $db = new Oracle();
+        $db->connect(USERNAME, PASSWORD);
+
+        $query = 'SELECT password
+                    FROM "user"
+                  WHERE username = :username';
+		$result = $db->qin($query, ["username" => $username]);
+		if(($result = self::fetch($result)) == false)
+			return false;
+		
+		if (password_verify($password, $result[0]["PASSWORD"]))
+		{
+			$query = 'SELECT NAME, LAST_NAME, EMAIL FROM "user" WHERE USERNAME = :username';
+			$result = $db->qin($query, ["username"=>$username]);
+			if(($result = self::fetch($result)) == false)
+				return false; // no sense ? 
+			$first_name = $result[0]["NAME"];
+			$last_name = $result[0]["LAST_NAME"];
+			$email = $result[0]["EMAIL"];
+
+			return new User($username, $first_name, $last_name, $email, "");
+		}else{
+			return false;
+		}
+    }
+
+    /**
+     *
+     */
+    public function register(): bool
+    {
+        $db = new Oracle();
+		$db->connect(USERNAME, PASSWORD);
+		$query = 'INSERT into "user" VALUES (
+			:username,
+			:password,
+			:first_name,
+			:last_name,
+			:email
+		  )';
+		$data = [
+			"username" => $this->username,
+			"password" => $this->password,
+			"first_name"=> $this->first_name,
+			"last_name" => $this->last_name,
+			"email" => $this->email
+		];
+		$result = $db->qout($query, $data);
+		if($result["success"] == false){
+			throw new DBCException($result["data"], 1);
+		}
+		return true;
+    }
+
+    /**
+     * admin -> { admin , client }
+     * client -> {admin}
+     */
+    public function contact(): void
+    {
+        // TODO: implement here
+    }
+
+    /**
+     *
+     */
+    public function add_video(): void
+    {
+        // TODO: implement here
 	}
 
-
 	/**
-	 * @var string
+	 * 
+	 * @throws DBCException 
 	 */
-	public $username;
-
-	/**
-	 * @var string
-	 */
-	private $first_name;
-
-	/**
-	 * @var string
-	 */
-	private $last_name;
-
-	/**
-	 * @var string
-	 */
-	private $email;
-
-	/**
-	 * @var string
-	 */
-	private $password;
-
-
-
-	/**
-	 *
-	 */
-	public function search():void
-	{
-		// TODO: implement here
-	}
-
-	/**
-	 *
-	 */
-	public function login():void
-	{
-		// TODO: implement here
-	}
-
-	/**
-	 *
-	 */
-	public function register():void
-	{
-		// TODO: implement here
-	}
-
-	/**
-	 * admin -> { admin , client }
-	 * client -> {admin}
-	 */
-	public function contact():void
-	{
-		// TODO: implement here
-	}
-
-	/**
-	 *
-	 */
-	public function add_video():void
-	{
-		// TODO: implement here
+	public static function fetch(array $result){
+		if(empty($result["success"])){
+			throw new DBCException("no result found");
+		}
+		else if(empty($result["data"])){
+			return false;
+		}
+		else{
+			return $result["data"];
+		}
 	}
 }
